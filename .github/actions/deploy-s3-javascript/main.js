d4884566891569268cfed34187babc7e9f3d35bf
Pedro Almeida <pedro@MBP-de-Pedro.Home>
2023-10-24 23:14:02 +0100

    quick fix visit url of s3 public url generated in action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -15,7 +15,7 @@ function run() {
     exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
     core.notice("Hello from my custom js action!")
 
-    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    const websiteUrl = `http://${bucket}.s3-website.${bucketRegion}.amazonaws.com`
     core.setOutput('website-url', websiteUrl)
 }
 
