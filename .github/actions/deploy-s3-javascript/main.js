f7e4c81d25abd32e6a19cfeff5d25a3216abb7ba
Pedro Almeida <pedro@MBP-de-Pedro.Home>
2023-10-24 22:50:20 +0100

    Added custom deploy action - deploy phase

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,16 @@ const github = require("@actions/github")
 const exec = require("@actions/exec")
 
 function run() {
+    // get some input values
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+
+    // upload my files
+    const s3URI = `s3://${bucket}`
+    // aws cli comes with the action runner base packages
+    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
     core.notice("Hello from my custom js action!")
 }
 
